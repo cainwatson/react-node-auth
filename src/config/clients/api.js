834739901e1930949151b1a312ab014772fa3153
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-04-17 15:15:09 -0500

    Install and configure ky http client

---
+++ src/config/clients/api.js
@@ -0,0 +1,6 @@
+import ky from 'ky';
+import API_CLIENT_URL from '../index';
+
+export const api = ky.create({
+  prefixUrl: API_CLIENT_URL,
+});
---
+++ src/config/index.js
@@ -0,0 +1,2 @@
+export const API_CLIENT_URL =
+  process.env.API_CLIENT_URL ?? 'http://localhost:3000/api';
