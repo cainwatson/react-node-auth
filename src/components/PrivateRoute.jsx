6536fc53381916d12c1e0783d359c6ec63c42717
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2021-04-17 18:30:46 -0500

    Adjust format of PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,22 +4,19 @@ import { useAuth } from '../hooks/useAuth';
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   const { isAuthenticated } = useAuth();
+  const render = (props) => {
+    if (isAuthenticated) {
+      return <Component {...props} />;
+    }
+    return (
+      <Redirect
+        to={{
+          pathname: `/signin`,
+          state: { from: props.location },
+        }}
+      />
+    );
+  };
 
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: `/signin`,
-              state: { from: props.location },
-            }}
-          />
-        )
-      }
-    />
-  );
+  return <Route {...rest} render={render} />;
 }
